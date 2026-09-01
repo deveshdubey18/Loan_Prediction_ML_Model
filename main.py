9b2ac1e77f55d89d7dc9e2548a63daa5dcddded3
Devesh R. Dubey <314devesh11scic@gmail.com>
2026-09-01 16:28:23 +0530

    Integrate model clustering into main function
    Added model clustering functionality to main.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.loan_prediction_classification_model.data_ingestion import data_ingestion
 from src.loan_prediction_classification_model.data_preprocessing import preprocessing
 from src.loan_prediction_classification_model.model_building import model_build
+from src.loan_prediction_classification_model.model_cluster import model_cluster
 
 def main():
     df = data_ingestion()
@@ -10,5 +11,8 @@ def main():
     
     report = model_build(X_train,X_test,y_train,y_test)
     print(report)
-    
+
+    kmeans= model_cluster(X_train,X_test)
+    print(kmeans)
+
 main()
